1884d9c9dea3a4c3dd85a3679bed323c60a05599
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-03-25 16:29:55 +0600

    version-structure

---
+++ django/utils/version.py
@@ -0,0 +1,29 @@
+import datetime
+import functools
+import os
+import subprocess
+import sys
+from distutils.version import LooseVersion
+
+PY36 = sys.version_info >= (3, 6)
+PY37 = sys.version_info >= (3, 7)
+PY38 = sys.version_info >= (3, 8)
+PY39 = sys.version_info >= (3, 9)
+
+def get_version(version=None):
+    pass
+
+def get_main_version():
+    pass
+
+def get_complete_version():
+    pass
+
+def get_docs_version():
+    pass
+
+def get_git_changeset():
+    pass
+
+def get_version_tuple(version):
+    pass
